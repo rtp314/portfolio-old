ffdb64947088b80121a841657461596fac34e548
Samuel Reed <samuelreed01@gmail.com>
2022-08-20 20:22:52 +0700

    split stylesheets

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 //ANIMATIONS
 
 const isReduced =
-    window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
-    window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+	window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
+	window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
 
 if (!isReduced) {
-    typingAnimation(document.querySelector(".subtitle"), 100);
+	typingAnimation(document.querySelector(".subtitle"), 100);
 }
 
 function typingAnimation(el, delay) {
-    function blinkCursor() {
-        return new Promise((resolve) => {
-            setTimeout(() => (cursor.style.opacity = 0), 500);
-            setTimeout(() => (cursor.style.opacity = 1), 1000);
-            setTimeout(() => (cursor.style.opacity = 0), 1500);
-            setTimeout(() => {
-                cursor.style.opacity = 1;
-                resolve();
-            }, 2000);
-        });
-    }
+	function blinkCursor() {
+		return new Promise((resolve) => {
+			setTimeout(() => (cursor.style.opacity = 0), 500);
+			setTimeout(() => (cursor.style.opacity = 1), 1000);
+			setTimeout(() => (cursor.style.opacity = 0), 1500);
+			setTimeout(() => {
+				cursor.style.opacity = 1;
+				resolve();
+			}, 2000);
+		});
+	}
 
-    function typing() {
-        return new Promise((resolve) => {
-            for (let i = 0; i < contentsArray.length; i++) {
-                setTimeout(() => {
-                    cursor.before(contentsArray[i]);
-                    if (i === contentsArray.length - 1) {
-                        resolve();
-                    }
-                }, i * delay);
-            }
-        });
-    }
+	function typing() {
+		return new Promise((resolve) => {
+			for (let i = 0; i < contentsArray.length; i++) {
+				setTimeout(() => {
+					cursor.before(contentsArray[i]);
+					if (i === contentsArray.length - 1) {
+						resolve();
+					}
+				}, i * delay);
+			}
+		});
+	}
 
-    const contentsArray = el.innerText.split("").map((letter) => {
-        const newSpan = document.createElement("span");
-        newSpan.innerText = letter;
-        return newSpan;
-    });
-    const cursor = document.createElement("span");
-    cursor.innerHTML = "█";
-    // cursor.style.fontSize = "0.9em";
-    el.innerText = "";
-    el.appendChild(cursor);
-    blinkCursor()
-        .then(typing)
-        .then(blinkCursor)
-        .then(blinkCursor)
-        .then(() => cursor.remove());
+	const contentsArray = el.innerText.split("").map((letter) => {
+		const newSpan = document.createElement("span");
+		newSpan.innerText = letter;
+		return newSpan;
+	});
+	const cursor = document.createElement("span");
+	cursor.innerHTML = "█";
+	// cursor.style.fontSize = "0.9em";
+	el.innerText = "";
+	el.appendChild(cursor);
+	blinkCursor()
+		.then(typing)
+		.then(blinkCursor)
+		.then(blinkCursor)
+		.then(() => cursor.remove());
 }
 
 //PROJECT DISPLAY LOGIC
@@ -63,29 +63,30 @@ closableInfo.forEach((div) => div.addEventListener("click", () => (div.style.dis
 buttons.forEach((el) => el.addEventListener("click", handleProjectChange));
 
 function handleProjectChange(e) {
-    function closeOthers(focused) {
-        displays.forEach((element) => {
-            if (element === focused) {
-                element.style.display = "block";
-                element.scrollIntoView({ behavior: "smooth" });
-            } else {
-                element.style.display = "none";
-            }
-            closableInfo.forEach((div) => (div.style.display = "block"));
-        });
-    }
+	const projectName = e.currentTarget.id;
+	const projectDisplay = document.getElementById(projectName + "-display");
+	closeOthers(projectDisplay);
+	setSelectedButton(e.currentTarget);
 
-    function setSelectedButton(el) {
-        buttons.forEach((button) => {
-            if (button === el) {
-                button.classList.add("selected");
-            } else {
-                button.classList.remove("selected");
-            }
-        });
-    }
-    const projectName = e.currentTarget.id;
-    const projectDisplay = document.getElementById(projectName + "-display");
-    closeOthers(projectDisplay);
-    setSelectedButton(e.currentTarget);
+	function closeOthers(focused) {
+		displays.forEach((element) => {
+			if (element === focused) {
+				element.style.display = "block";
+				element.scrollIntoView({ behavior: "smooth" });
+			} else {
+				element.style.display = "none";
+			}
+			closableInfo.forEach((div) => (div.style.display = "block"));
+		});
+	}
+
+	function setSelectedButton(el) {
+		buttons.forEach((button) => {
+			if (button === el) {
+				button.classList.add("selected");
+			} else {
+				button.classList.remove("selected");
+			}
+		});
+	}
 }
